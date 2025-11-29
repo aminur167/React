eeda87ee526fd3f16f627ef20b8511fc60c6e564
aminur167 <aminurhstu23@gmail.com>
2025-11-29 11:18:24 +0600

    react

--- 01/my-app/src/App.js
+++ 01/my-app/src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Feedback from './pages/Feedback';
 import Home from './pages/Home';
 
 function App() {
@@ -6,6 +9,9 @@ function App() {
     <div className="App">
       <h1>Hello Word!</h1>
       <Home/>
+      <About/>
+      <Contact/>
+      <Feedback/>
     </div>
   );
 }
