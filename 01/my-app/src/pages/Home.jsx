950745acdca27a41a808f188b3221dfad68d131c
aminur167 <aminurhstu23@gmail.com>
2025-11-29 09:59:15 +0600

    React

---
+++ 01/my-app/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1>I love you sadia</h1>
+      <h2>You are my queen</h2>
+      <h3>Miss you my cutipie</h3>
+      <h4>You are my hart</h4>
+    </div>
+  )
+}
+
+export default Home
